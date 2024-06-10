65a7fd6dbb7540936427cc7164f10788a6a7f29c
Vladimir Laskin <148013724+Viktotovich@users.noreply.github.com>
2024-06-10 04:43:37 +0000

    main library object made

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ goToLibraryButton.addEventListener("click", goToLibrary());
 function goToLibrary() {
 return library.scrollIntoView();
 }
+
+/* MVP - Library cards */ 
+function Book(name, title, author, year, read) {
+    this.name = name,
+    this.title = title,
+    this.author = author,
+    this.year = year
+}
+
+// more elegant to add it to the book __Proto__
+Book.prototype.read = function() {
+        if (!read) {
+            // add text readDom.textContent = 'Not read yet' 
+        } else {
+            // add text readDom.textContent = 'Read' 
+        }
+    }
+
+atomicHabits = new Book();
