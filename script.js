aeac7e619ec4ec963348e85666b3145b84637dd2
Vladimir Laskin <148013724+Viktotovich@users.noreply.github.com>
2024-06-11 06:23:27 +0400

    <!DOCTYPE html> <html lang="en"> <head>     <meta charset="UTF-8">     <meta name="viewport" content="width=device-width, initial-scale=1.0">     <title>Document</title>     <link rel="stylesheet" href="styles.css">     <script src="script.js" defer></script> </head> <body>     <!--Top Body (no header for this one)-->     <div class="body-container">         <h1><em>Tap into your inner librarian</em></h1>         <div class="flavor-text">A limitless library made by you, designed by you - with all your favorite authors and books...</div>         <button id="anchor-to-library" type="button">Start adding books!</button>     </div>
        <!--mid body-->
        <div class="featured-book-container">
            <img src="" alt="featured book">
            <div class="book-description-container">
                <h2 id="book"></h2>
                <div class="book-description"></div>
                <button type="button" id="next">
                </button>
            </div>
        </div>
    
        <!--library grid -->
        <div class="library-container">
            <h3>You Library, Full Control</h3>
    
            <div class="book-description-container">
                <h2 id="book"></h2>
                <div class="book-description"></div>
                <button type="button" id="next" class="more-details">More Details
                </button>
            </div>
    
            <div class="book-description-container">
                <h2 id="book"></h2>
                <div class="book-description"></div>
                <button type="button" id="next" class="more-details">More Details
                </button>
            </div>
    
            <div class="user-book-container">
                <button class="add-book" style="font-size: 5rem; color: greenyellow;">[+]</button>
                <dialog>
                    <form action="" method="">
                        <div>
                            <label for="title">Book Title:</label>
                            <input id="title" type="text" name="title">
                        </div>
    
                        <div>
                            <label for="author">Author Name:</label>
                            <input id="author" type="text" name="author">
                        </div>
    
                        <div>
                            <label for="year">Year of release:</label>
                            <input id="year" type="year" name="year">
                        </div>
    
                        <div>
                            <label for="pages">Pages:</label>
                            <input id="pages" type="number" name="pages">
                        </div>
    
                        <div>
                            <label for="userRead">Did you read it?</label>
                            <input id="userRead" type="checkbox" name="userRead">
                        </div>
                    </form>
                </dialog>
            </div>
        </div>
    </body>
    </html>

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Anchor to scroll - add slow scrolling later on 
+// Anchor to scroll - add slow scrolling later on
 const goToLibraryButton = document.getElementById("anchor-to-library");
 const libraryContainer = document.querySelector(".library-container");
 
@@ -9,22 +9,23 @@ function goToLibrary() {
 return libraryContainer.scrollIntoView();
 }
 
-/* MVP - Library cards */ 
+/* MVP - Library cards */
 const library = [];
 
-function Book(title, author, year, pages) {
+function Book(title, author, year, pages, read) {
     this.title = title,
     this.author = author,
     this.year = year,
-    this.pages = pages
+    this.pages = pages,
+    this.read = read
 }
 
-// more elegant to add it to the book __Proto__
+// more elegant to add it this way rather than __Proto__ + the latter is obsolete
 Book.prototype.read = function() {
-        if (!read) {
-            // add text readDom.textContent = 'Not read yet' 
+        if (!this.read) {
+            // add text readDom.textContent = 'Not read yet'
         } else {
-            // add text readDom.textContent = 'Read' 
+            // add text readDom.textContent = 'Read'
         }
     }
 
@@ -33,11 +34,11 @@ const libroRojo = new Book("El pequeño libro rojo de las ventas", 'Fran Rodrigu
 
 library.splice(0, 0, atomicHabits, libroRojo);
 
-// DOM - user content 
+// DOM - user content
 //need an SVG with a plus that gets shifted / deleted anytime a user adds a new book
 
 function addToLibrary() {
-    
+
 }
 
 //how to turn the obj's to an array
