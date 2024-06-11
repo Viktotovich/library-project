a274268c05d1de3359cb9405d796ac8abdce226a
Vladimir Laskin <148013724+Viktotovich@users.noreply.github.com>
2024-06-11 06:53:54 +0400

    "Testing the algorithm of re-iterating over the obj constructor to get the books - finding what enumerable value I can use to make an array of books"

--- script.js
+++ script.js
@@ -16,8 +16,7 @@ function Book(title, author, year, pages, read) {
     this.title = title,
     this.author = author,
     this.year = year,
-    this.pages = pages,
-    this.read = read
+    this.pages = pages
 }
 
 // more elegant to add it this way rather than __Proto__ + the latter is obsolete
@@ -32,13 +31,22 @@ Book.prototype.read = function() {
 const atomicHabits = new Book("Atomic Habits", "James Clear", 2018, 277);
 const libroRojo = new Book("El pequeño libro rojo de las ventas", 'Fran Rodrigue', 2004, 224);
 
+
+// loop to add things in the object to the library 
+function addToLibrary() {
+    for (const book in Book) {
+        console.log("book x")
+        return book
+    }
+}
+
 library.splice(0, 0, atomicHabits, libroRojo);
+
+
 
 // DOM - user content
 //need an SVG with a plus that gets shifted / deleted anytime a user adds a new book
 
-function addToLibrary() {
 
-}
 
 //how to turn the obj's to an array
